33a7af3a54411b816bb44271c020aaeaeb053b0e
davidwinsen7 <davidwinsen7@users.noreply.github.com>
2020-11-24 00:19:28 +0800

    Design Update and Prototyping
    - Styling for tabs, labels, canvas
    - App logo
    - game library concept (Not Final Yet)

---
+++ ID Game Launcher/ID Game Launcher/Launcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ID_Game_Launcher
+{
+    class Launcher
+    {
+        public static void PlayGame(string fileName)
+        {
+            Process.Start(fileName);
+        }
+
+    }
+}
--- ID Game Launcher/ID Game Launcher/MainWindow.xaml.cs
+++ ID Game Launcher/ID Game Launcher/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,24 @@ namespace ID_Game_Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        string AgentBabyDesc = "Play as Agent Baby! a secret agent who possesses a powerful weapon that can rewind people to younger age! Infiltrate the evil lair and make your way to the boss in this fun little 2d platformer!";
+
         public MainWindow()
         {
             InitializeComponent();
+            GameDesc.Text = AgentBabyDesc;
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Directing to game library!");
+            MenuTab.SelectedIndex = 1;
+            
+        }
+
+        private void PlayAgentBaby_Click(object sender, RoutedEventArgs e)
+        {
+            //Launcher.PlayGame(gameExe);
         }
     }
 }
