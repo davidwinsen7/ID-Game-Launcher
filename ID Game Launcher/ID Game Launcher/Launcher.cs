5aed492a21f3107faf8e89a3616fb9488da52d8c
davidwinsen7 <davidwinsen7@users.noreply.github.com>
2020-11-25 16:54:18 +0800

    Revert "Launcher class : contains function to launch games"
    This reverts commit 1e0d26ee09bb7b1baeee1a3b11d8cad16061317d.

--- ID Game Launcher/ID Game Launcher/Launcher.cs
+++ ID Game Launcher/ID Game Launcher/Launcher.cs
@@ -4,44 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-using Path = System.IO.Path;
-using System.IO;
 
 namespace ID_Game_Launcher
-{   
+{
     class Launcher
     {
-        private static string rootDirectory;
-        private static string gameDirectory;
-        private static string gameExe;
-
-        private static string gameFolder;
-        private static string gameExeName;
         public static void PlayGame(string fileName)
         {
-            switch (fileName)
-            {
-                case "AgentBaby":
-                    gameFolder = "Agent Baby";
-                    gameExeName = "Agent Baby.exe";
-                    break;
-                case "SnowYard":
-                    gameFolder = "Snow-Yard";
-                    gameExeName = "Snow-Yard.exe";
-                    break;
-            }
-
-            rootDirectory = Directory.GetCurrentDirectory();
-            gameDirectory = Path.Combine(rootDirectory, "Games");
-            gameExe = Path.Combine(gameDirectory, gameFolder, gameExeName);
-
-            if (File.Exists(gameExe))
-            {
-                ProcessStartInfo startInfo = new ProcessStartInfo(gameExe);
-                startInfo.WorkingDirectory = Path.Combine(gameDirectory, "Agent Baby");
-                Process.Start(startInfo);
-            }
-            //Process.Start(fileName);
+            Process.Start(fileName);
         }
 
     }
